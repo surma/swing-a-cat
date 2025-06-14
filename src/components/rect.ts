b313711f3c3be0b6b90a1d5d1e6df75465cd8060
Surma <surma@surma.dev>
2025-06-14 23:37:09 +0100

    Remove stray

--- src/components/rect.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "../scene";
-
-export interface RectComponent extends Component {
-  type: "rect";
-  x: number;
-  y: number;
-  w: number;
-  h: number;
-}
--- src/entities/rope.ts
+++
@@ -1,7 +0,0 @@
-import { Ctx } from "../game";
-import { addToChildren } from "../scene";
-
-export function rope({ x, y }, ctx: Ctx) {
-  // addToChildren(
-  // ctx.scene
-}
